01d222562f801ccb88faf3039f64d4f8818f8406
Jose M. Gomez L <manuelgomezjp@gmail.com>
2023-12-31 13:25:59 -0500

    Se agrega angular material

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    quotesUrl: 'http://localhost:8080/quote'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    quotesUrl: 'https://quotes-api-100.ue.r.appspot.com/quote'
+};
