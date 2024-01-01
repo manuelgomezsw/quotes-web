2676ee463df6003b8b29f30d67129f1f9022a5e4
Jose M. Gomez L <manuelgomezjp@gmail.com>
2024-01-01 14:50:23 -0500

    Se genera esqueleto gráfico del sitio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatExpansionModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'quotes-web';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { NewComponent } from './new/new.component';
+import { EditComponent } from './edit/edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'search',
+    title: 'Search Quotes',
+    component: SearchComponent,
+  },
+  {
+    path: 'new',
+    title: 'New Quote',
+    component: NewComponent
+  },
+  {
+    path: 'edit',
+    title: 'Edit Quote',
+    component: EditComponent
+  },
+];
